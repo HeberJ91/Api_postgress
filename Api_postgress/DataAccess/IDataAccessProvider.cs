ed92d02d88cf83e3a596ec3e0b2f4a420c4520d2
hmurillo <heber_jose@hotmail.com>
2020-10-15 10:01:53 -0600

    Add project files.

---
+++ Api_postgress/DataAccess/IDataAccessProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_postgress.Models;
+
+namespace Api_postgress.DataAccess
+{
+ 
+    public interface IDataAccessProvider
+    {
+        void AddPatientRecord(Patient patient);
+        void UpdatePatientRecord(Patient patient);
+        void DeletePatientRecord(string id);
+        Patient GetPatientSingleRecord(string id);
+        List<Patient> GetPatientRecords();
+    }
+}
---
+++ Api_postgress/DataAccess/PostgreSqlContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api_postgress.Models;
+
+namespace Api_postgress.DataAccess
+{
+    public class PostgreSqlContext: DbContext
+    {
+        public PostgreSqlContext(DbContextOptions<PostgreSqlContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+            return base.SaveChanges();
+        }
+    }
+}
